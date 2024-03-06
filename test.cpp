5099174288e37cd00deda5e4274bc0425e0b2a87
flution <flution221@gmail.com>
2024-03-06 15:33:12 +0900

    docs : 24-03-06 C언어 학습(배열까지)

--- test.cpp
+++ test.cpp
@@ -1,3 +1,15 @@
 #include<stdio.h>
 
-void
+void main()
+{
+	int array[5];
+
+	for (int i = 0; i < 5; i++)
+	{
+		printf("정수를 입력하세요 : ";
+		scanf_s("%d", &array[i]);
+
+	}
+
+	printf("배열 요소의 출력 : %d, %d, %d, %d, %d\n", array[1], array[2], array[3], array[4], array[5])
+}
