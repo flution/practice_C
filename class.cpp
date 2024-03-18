1013775b7b2bc75e546de9f8109338a8a6f85909
flution <flution221@gmail.com>
2024-03-18 16:13:45 +0900

    docs : 240318 C++언어( 스태틱 까지 )

--- class.cpp
+++ class.cpp
@@ -56,27 +56,96 @@ public:
 		cout << " 소멸자 " << endl;
 	}
 
-	int GetX() {
+	int GetX() const {
+
 		return x;
 	}
 
-	int GetY() {
+	int GetY() const {
 		return y;
 	}
 
-	void SetXY(int nX, int nY) {
-		x = nX;
-		y = nY;
+	void SetXY(int x, int y) {
+			
+			this->x = x;
+			this->y = y;
 	}
 };
 
+//얕은 복사의 내용
+void SetRect(MousePoint pt1, MousePoint pt2)
+{
+	cout << pt1.GetX() << endl;
+	cout << pt2.GetX() << endl;
+	cout << pt1.GetY() << endl;
+	cout << pt2.GetY() << endl;
+	pt1.SetXY(1000, 2000);
+	cout << pt1.GetX() << endl;
+	cout << pt1.GetY() << endl;
+}
+
+MousePoint& CopyObject(MousePoint& pt1, MousePoint& pt2)
+{
+	pt1 = pt2;
+	cout << pt1.GetX() << endl;
+	cout << pt1.GetY() << endl;
+	return pt1;
+}
+
 void main()
 {
-	/*MousePoint pt = { 50, 100 };*/
-	MousePoint pt(10, 20);
-	MousePoint a; //이건 왜 오류냐? = 원인은 전달인자가 2개인 생성자 떄문이다.
-	 
 
-	cout << pt.GetX() << endl;
-	cout << pt.GetY() << endl;
+	MousePoint mp1(10, 20);
+	MousePoint mp2(50, 60);
+	CopyObject(mp1, mp2);
+	cout << mp1.GetX() << endl;
+	cout << mp1.GetY() << endl;
+
+
+
+	///*MousePoint pt = { 50, 100 };*/
+	//MousePoint pt(10, 20);
+	//MousePoint a; //이건 왜 오류냐? = 원인은 전달인자가 2개인 생성자 떄문이다.
+	// 
+
+	//cout << pt.GetX() << endl;
+	//cout << pt.GetY() << endl;
+
+	/*MousePoint pt[3] = { MousePoint(10, 20), MousePoint(100,200), MousePoint(500,600) };*/
+	//배열을 초기화 하는 방법.
+	
+	/*MousePoint pt[3] = { 10, 20, 30 */
+	//정수형 배열이 아니므로 { x , y , z }는 불가능하다.
+	
+	//이건 직접접근연산자다. 알겠나?
+	//pt[0].SetXY(10, 20);
+	//pt[1].SetXY(100, 200);
+	
+	//for문 돌려서 편해지기.
+	/*for (int i = 0; i < 3; i++) 
+	{
+	cout << pt[i].GetX() << endl;
+	cout << pt[i].GetY() << endl;
+	}*/
+
+	//이건 객체 포인터를 통한 "간접접근 연산자"
+	/*MousePoint* pObj;
+	MousePoint pt(10, 20);*/
+
+	//pObj = &pt;
+	//pt.SetXY(200, 300);
+	////특히 -> 를 통해서 pObj(는 pt의 값을 참조하는 것) 의 값을 GetX에 넣겠다. 라는 거임.
+	//cout << pObj->GetX() << endl;
+	//cout << pObj->GetY() << endl;
+
+
+	//얕븐 복사값에 대한 이해.
+	/*MousePoint* pObj;
+	MousePoint mp1(10, 20);
+	MousePoint mp2(50, 60);
+
+	SetRect(mp1, mp2);
+	cout << mp1.GetX() << endl;
+	cout << mp1.GetY() << endl;*/
+
 }
---
+++ 예금계좌.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+
+class Deposit //예금.
+{
+	private:
+		char* strName; //예금 주
+		double dBalance; //예금 잔액
+		static double dInterestRate; //예금 금리는 상품마다 고정이니까 static을 붙여준다.
+
+	public:
+		Deposit() {}
+		Deposit(char* name, double balance) //예금주랑 예금잔액을 끌어올 생성자.
+		{
+			strName = name;
+			dBalance = balance;
+		}
+		void BankBalance() // 은행 잔액
+		{
+			dBalance = dBalance + (dBalance * dInterestRate);
+		}
+		static void SetInterestRate(double dNewRate)
+		{
+			dInterestRate = dNewRate;
+		}
+};
+
+double Deposit::dInterestRate = 0.05;
+//이걸 해줌으로써 dInterestRate 가 0.05로 계속 초기화 된다. 이건가?
+
+void main()
+{
+	Deposit Kim, lee;
+	//Public의 경우 오류가 안나더라도 이렇게 쓰면 빵점입니다~
+	Deposit::SetInterestRate(0.06);
+	//스태틱을 바꾸는 방법은 뒤에 static 변수를 또 만들어서 치환하는 방법 뿐임.
+	Kim.SetInterestRate(0.07);
+}
